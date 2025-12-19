1a91d5bb3ee44fbb32578d76913d4ff31ef072ac
patelsamerth9 <patelsamerth9@gmail.com>
2025-12-19 21:11:42 +0530

    Add files via upload
    If else condition check

---
+++ string concatenation.py
@@ -0,0 +1,12 @@
+s1 = 10
+s2 = 20
+s3 = s1 + s2
+print(s3)
+s4 = "hello "
+s5 = "world"
+s6 = s4 + s5
+print(s6)
+s7 = "The sum of " + str(s1) + " and " + str(s2) + " is " + str(s3) + "." + str(s4) + str(s5) + "!"
+print(s7)
+s = 'danger' 
+print(s)
