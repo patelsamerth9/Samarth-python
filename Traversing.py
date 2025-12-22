2e509a544784266412570db993bd66d5e573d8f2
Ravendra Patel <patelsamerth9@gmail.com>
2025-12-23 00:22:04 +0530

    Add files via upload

---
+++ Traversing.py
@@ -0,0 +1,5 @@
+x = 'hello world and how are you'
+for i in range(len(x)):# Using range and len to traverse the string
+    print(x[i])     # Printing each character at index i
+    #with this method we can also access the index of each character easily same as the the array
+    print(i)# Printing the index of each character(this line is only used to show index number and its optional)
---
+++ string lateral.py
@@ -0,0 +1,10 @@
+s1='hello samarth'# single quote string
+s2="hello samarth"# double quote string
+s3='''hello samarth'''# triple single quote string
+s4="""hello samarth"""# triple double quote string
+s5="""hello samarth"""# triple double quote string
+print(s1)#In python we can use single,double,triple single and triple double quotes to define string
+print(s2)#we can use any of them as per our convenience
+print(s3)#Triple single quotes can be used for multi-line strings
+print(s4)#Triple double quotes can also be used for multi-line strings
+print(s5)#we can write multi-line string in any of the triple quotes
